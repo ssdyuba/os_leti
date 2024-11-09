c87b01f445a7a32f65c64ecc0dbeed097ecbfbb9
LtFrost <thefrostsg@gmail.com>
2024-11-09 20:06:07 +0300

    edit

--- lab1/src/main.rs
+++ lab1/src/main.rs
@@ -4,13 +4,17 @@ use std::thread;
 use std::time::Duration;
 use std::io::Write;
 
-fn thread_func(number: char, stop_flag: Arc<AtomicBool>) -> i32 {
+fn thread_func(number: char, stop_flag: Arc<AtomicBool>) -> i32 {  //Пришлось использовать атомные
+                                                                   //переменные, с которыми пока не
+                                                                   //до конца разобрался т.к.
+                                                                   //переменные bool нельзя
+                                                                   //передавать между потоками
+    print!("\nПоток {} начал работу\n", number);
     while !stop_flag.load(Ordering::Relaxed) {
         print!("{}", number);
-        std::io::stdout().flush().unwrap(); // Чтобы сразу выводить символы на экран
+        std::io::stdout().flush().unwrap();      // Вывод цифр на экран
         thread::sleep(Duration::from_secs(1));
     }
-    // Код завершения потока
     if number == '1' {
         1
     } else {
@@ -28,19 +32,19 @@ fn main() {
     let handle1 = thread::spawn(move || thread_func('1', flag_1));
     let handle2 = thread::spawn(move || thread_func('2', flag_2));
 
-    // Ожидаем нажатия клавиши
-    println!("Press Enter to stop...");
+    thread::sleep(Duration::from_millis(200)); //Небольшой костыль, чтобы сообщение о клавише Enter
+                                               //выводилось после сообщения о запуске потоков
+    println!("\nНажмите Enter, чтобы остановить выполнение...");
     let _ = std::io::stdin().read_line(&mut String::new()).unwrap();
+    print!("Клавиша нажата");
 
-    // Устанавливаем флаги завершения для обоих потоков
     stop_flag_1.store(true, Ordering::Relaxed);
     stop_flag_2.store(true, Ordering::Relaxed);
 
-    // Ожидаем завершения потоков и получаем коды завершения
     let exit_code_1 = handle1.join().unwrap();
     let exit_code_2 = handle2.join().unwrap();
 
-    println!("\nThread 1 exited with code: {}", exit_code_1);
-    println!("Thread 2 exited with code: {}", exit_code_2);
+    println!("\nПоток {} завершил работу", exit_code_1);
+    println!("Поток {} завершил работу", exit_code_2);
 }
 
